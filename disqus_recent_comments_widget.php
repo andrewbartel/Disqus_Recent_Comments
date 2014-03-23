52523832ec793c76e5976e83312eafa4aff14398
rkmd <rishi@rk.md>
2014-03-23 09:56:58 -0500

    Update disqus_recent_comments_widget.php

--- disqus_recent_comments_widget.php
+++ disqus_recent_comments_widget.php
@@ -244,13 +244,11 @@ class disqus_recent_comments_widget extends WP_Widget {
 	}
 
 	protected function echo_comments($comment, $api_key, $style_params,$args=false) {
-		
 		$cached = get_transient($YOURDISQUS);
 		if (false !== $cached){
 			echo $cached;
 			return;
 		}
-		
 		extract($args);
 		//basic counter
 		$comment_counter = 0;
@@ -367,6 +365,7 @@ class disqus_recent_comments_widget extends WP_Widget {
 		$recent_comments .= $after_widget;
 
 		$recent_comments = apply_filters( 'disqus_rcw_recent_comments' , $recent_comments );
+		
 		set_transient($YOURDISQUS, $recent_comments, 600);
 		echo($recent_comments);
 	}
